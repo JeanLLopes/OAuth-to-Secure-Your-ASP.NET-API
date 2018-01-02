1bf5c1fa86770533c62849708b531edaaf907754
Jean Lopes <jeean.llopes@outlook.com>
2018-01-02 17:21:15 -0200

    Add Startup

---
+++ SocialNetwork.OAuthServer/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.Core.Configuration;
+using System.Configuration;
+
+[assembly: OwinStartup(typeof(SocialNetwork.OAuthServer.Startup))]
+
+namespace SocialNetwork.OAuthServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            //GET THE CERTIFICATE FROM web.config
+            var certificate = Convert.FromBase64String(ConfigurationManager.AppSettings["SigningCertificate"]);
+
+            var optionsIdentityServer = new IdentityServerOptions()
+            {
+                SigningCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2()
+            };
+
+            app.UseIdentityServer()
+        }
+    }
+}
